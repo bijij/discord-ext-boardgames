1d6d8ee46c5497a2060eee56bf64c5fb17ce9d19
Josh B <josh.ja.butt@gmail.com>
2019-10-10 23:37:44 +1000

    Add new Column and Row converters.

--- discord/ext/boardgames/converter.py
+++ discord/ext/boardgames/converter.py
@@ -1,16 +1,44 @@
 import re
 
+from typing import Tuple
+
 from discord.ext import commands
 
-__all__ = ('Cell',)
+__all__ = ('Cell', 'Column', 'Row')
+
+
+class Column(commands.Converter):
+    """Returns the index of a column."""
+
+    @classmethod
+    def from_char(cls, argument: str) -> int:
+        return ord(argument.upper()) - ord('A')
+
+    @classmethod
+    def convert(cls, ctx: commands.Context, argument: str) -> int:
+        if re.match(r'[A-z]', argument):
+            return cls.from_char(argument)
+
+
+class Row(commands.Converter):
+    """Returns the index of a row."""
+
+    @classmethod
+    def from_char(cls, argument: str) -> int:
+        return int(argument) - 1
+
+    @classmethod
+    def convert(cls, ctx: commands.Context, argument: str) -> int:
+        if re.match(r'\d+', argument):
+            return cls.from_char(argument)
 
 
 class Cell(commands.Converter):
-    """Returns the index of a cell."""
+    """Returns the index of a row and column."""
 
     @classmethod
-    async def convert(self, ctx: commands.Context, argument: str) -> str:
+    def convert(self, ctx: commands.Context, argument: str) -> Tuple[int, int]:
         if re.match(r'[A-z]\d+', argument):
-            return (int(argument[1:]) - 1, ord(argument[0].upper()) - 65)
+            return (Row.from_char(argument[1:]), Column.from_char(argument[0]))
 
         raise commands.BadArgument('Could not determine cell!')
